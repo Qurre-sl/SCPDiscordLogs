edabbebfb0c44734e8d7dffaffe51a7d061f8116
fydne <serverafydne@gmail.com>
2021-03-31 01:00:24 +0500

    .

---
+++ DiscordLogs.js
@@ -0,0 +1,41 @@
+module.exports = [
+	{
+		token: "",
+		port: 7777,
+		host: 'localhost',
+		name: "Light RP",
+		logs: "816260046547714070",
+		ralogs: "721380128432717935",
+		tklogs: "706170791028588644",
+		bklogs: "721377600672628736",
+		translate: {
+			ban: 'Ban',
+			ban_msg: '%banned% has been banned by %banner%. Reason: %reason%\nUntil %to%',
+			kick: 'Kick',
+			kick_msg: '%kicked% has been kicked by %kicker%. Reason: %reason%',
+			player: "player",
+			players: "players",
+			/* for ru(винительный падеж(игрока)) */
+			playera: "players"
+		}
+	},/*
+	{
+		token: "",
+		port: 7778,
+		host: 'localhost',
+		name: "ff:off",
+		logs: "816260046547714070",
+		ralogs: "721380128432717935",
+		tklogs: "706170791028588644",
+		bklogs: "721377600672628736",
+		translate: {
+			ban: 'Ban',
+			ban_msg: '%banned% has been banned by %banner%. Reason: %reason%\nUntil %to%',
+			kick: 'Kick',
+			kick_msg: '%kicked% has been kicked by %kicker%. Reason: %reason%',
+			player: "player",
+			players: "players",
+			playera: "players"
+		}
+	},*/
+]
