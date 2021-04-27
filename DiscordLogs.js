7750f474fa30089b7a36f91659da4841ddd04226
fydne <serverafydne@gmail.com>
2021-04-28 00:12:24 +0500

    1.1.0

--- DiscordLogs.js
+++ DiscordLogs.js
@@ -4,8 +4,10 @@ module.exports = [
 		port: 7777,
 		host: 'localhost',
 		name: "Light RP",
+		prefix: "$",
 		logs: "816260046547714070",
 		ralogs: "721380128432717935",
+		rasend: "817333851713175552",
 		tklogs: "706170791028588644",
 		bklogs: "721377600672628736",
 		translate: {
@@ -13,29 +15,37 @@ module.exports = [
 			ban_msg: '%banned% has been banned by %banner%. Reason: %reason%\nUntil %to%',
 			kick: 'Kick',
 			kick_msg: '%kicked% has been kicked by %kicker%. Reason: %reason%',
+			disconnect: '⚠️ Server disconnected.',
+			connect: '\\✔️ Server connected.',
 			player: "player",
 			players: "players",
 			/* for ru(винительный падеж(игрока)) */
-			playera: "players"
+			playera: "players",
+			no_server: 'server offline',
 		}
 	},/*
 	{
 		token: "",
-		port: 7778,
+		port: 7779,
 		host: 'localhost',
-		name: "ff:off",
-		logs: "816260046547714070",
-		ralogs: "721380128432717935",
-		tklogs: "706170791028588644",
-		bklogs: "721377600672628736",
+		name: "",
+		prefix: "$",
+		logs: "",
+		ralogs: "",
+		rasend: "",
+		tklogs: "",
+		bklogs: "",
 		translate: {
 			ban: 'Ban',
 			ban_msg: '%banned% has been banned by %banner%. Reason: %reason%\nUntil %to%',
 			kick: 'Kick',
 			kick_msg: '%kicked% has been kicked by %kicker%. Reason: %reason%',
+			disconnect: '⚠️ Server disconnected.',
+			connect: '\\✔️ Server connected.',
 			player: "player",
 			players: "players",
-			playera: "players"
+			playera: "players",
+			no_server: 'server offline',
 		}
 	},*/
 ]
